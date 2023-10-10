e74500fbe73505a87d525b8a5833991bae8eca35
Siddartha Yadav <officialsiddartha@gmail.com>
2023-10-10 22:34:30 +0530

    Update settings.py allowed everyojne

--- navbr/settings.py
+++ navbr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=97ldrww%nm=j!ddx_q5h$()r)lr&^g*otd64pj*jpb-*mgr+p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
